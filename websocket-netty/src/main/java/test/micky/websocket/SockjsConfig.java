0773a4211eb2fe1e501d734d13a64c2ab59fcf95
ksmail13 <ksmail13@gmail.com>
2019-01-09 02:10:57 +0900

    fix sockjs config - sockjs initialize handler use functional router

---
+++ websocket-netty/src/main/java/test/micky/websocket/SockjsConfig.java
@@ -0,0 +1,33 @@
+package test.micky.websocket;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+import test.micky.websocket.socket.SockjsOption;
+
+@Configuration
+public class SockjsConfig {
+
+	@Bean
+	public WebSocketHandler echoSocketHandler() {
+		return new EchoWebSocketHandler();
+	}
+
+	@Bean
+	public RouterFunction<?> sockjsInfo(Optional<SockjsOption> option) {
+		SockjsOption o = option.orElse(SockjsOption.builder().build());
+		return RouterFunctions.route().path(o.getPrefix(),
+				b -> b.GET("/info", req -> ServerResponse.ok().body(BodyInserters.fromObject(o))).GET("",
+						req -> ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).syncBody("Welcome to SockJs!\n")))
+				.build();
+	}
+
+}
--- websocket-netty/src/main/java/test/micky/websocket/socket/SockjsOption.java
+++ websocket-netty/src/main/java/test/micky/websocket/socket/SockjsOption.java
@@ -1,10 +1,9 @@
 package test.micky.websocket.socket;
 
-import java.util.Map;
-import java.util.Set;
-
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
 
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +12,10 @@ import lombok.Data;
 @Builder
 public class SockjsOption {
 	private static final Set<Transport> DEFAULT_TRANSPORT = Collections.unmodifiableSet(EnumSet.allOf(Transport.class));
-	
+
 	@Builder.Default
 	private final String prefix = "";
-	private final boolean websocket;
+	@Builder.Default
 	private final Set<Transport> transports = DEFAULT_TRANSPORT;
 	private final long responseLimit = 128 * 1024;
 	private final Map<String, Object> fayeServerOptions;
@@ -26,5 +25,8 @@ public class SockjsOption {
 	private final long disconnectDelay = 5000L;
 	@Builder.Default
 	private final String sockjsUrl = "https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js";
-	
+
+	public boolean isWebsocket() {
+		return transports.contains(Transport.WEBSOCKET) || transports.contains(Transport.WEBSOCKET_RAW);
+	}
 }
