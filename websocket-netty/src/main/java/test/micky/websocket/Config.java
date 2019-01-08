52715be7f6700419f174e73fc67172e8f14fe6a6
ksmail13 <ksmail13@gmail.com>
2019-01-09 02:09:35 +0900

    fix config - use boot webflux setting - spring logging level up

--- websocket-netty/src/main/java/test/micky/websocket/Config.java
+++ websocket-netty/src/main/java/test/micky/websocket/Config.java
@@ -1,27 +1,44 @@
 package test.micky.websocket;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@EnableWebFlux
 @Slf4j
-public class Config {
+public class Config implements WebFluxConfigurer {
 
-    @Bean
-    public WebSocketHandler echoSocketHandler() {
-        return new EchoWebSocketHandler();
-    }
+	@Autowired
+	private ThymeleafReactiveViewResolver viewResolver;
 
-    @Bean
-    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.failOnUnknownProperties(false);
-        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-        return builder;
-    }
+	@Bean
+	public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+		return new Jackson2ObjectMapperBuilder().failOnUnknownProperties(false)
+				.serializationInclusion(JsonInclude.Include.NON_NULL);
+	}
+
+	@Override
+	public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+		configurer.defaultCodecs().enableLoggingRequestDetails(log.isDebugEnabled());
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		if (viewResolver != null) {
+			log.error("viewResolver is emtpy");
+		}
+		registry.viewResolver(viewResolver);
+	}
 
 }
